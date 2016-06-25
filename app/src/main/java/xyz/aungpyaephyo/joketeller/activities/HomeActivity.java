aac23637a1237cefb61df87613aea15beef093cd
Aung Pyae Phyo <aungpyaephyo24@gmail.com>
2016-06-26 01:45:38 +0630

    - Event List Screen -> RecyclerView. - FAB - CoordinatorLayout -> (FAB ~ Snackbar | Scroll app bar off-screen when content scroll).

--- app/src/main/java/xyz/aungpyaephyo/joketeller/activities/HomeActivity.java
+++ app/src/main/java/xyz/aungpyaephyo/joketeller/activities/HomeActivity.java
@@ -2,20 +2,18 @@ package xyz.aungpyaephyo.joketeller.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.ShareCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.SubMenu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
@@ -23,11 +21,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import xyz.aungpyaephyo.joketeller.R;
+import xyz.aungpyaephyo.joketeller.fragments.EventFragment;
 import xyz.aungpyaephyo.joketeller.fragments.JokeFragment;
 import xyz.aungpyaephyo.joketeller.utils.JokeTellerConstants;
 import xyz.aungpyaephyo.joketeller.utils.MMFontUtils;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener,
+        MenuItemCompat.OnActionExpandListener {
 
     private int jokeIndex = -1;
 
@@ -37,8 +38,6 @@ public class HomeActivity extends AppCompatActivity {
     private FrameLayout flContainer;
     private Button btnNextJoke;
     private Button btnPreviousJoke;
-
-    private ShareActionProvider mShareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,36 +58,10 @@ public class HomeActivity extends AppCompatActivity {
         navigationView = (NavigationView) findViewById(R.id.navigation_view);
         Menu leftMenu = navigationView.getMenu();
         MMFontUtils.applyMMFontToMenu(leftMenu);
-
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override public boolean onNavigationItemSelected(MenuItem menuItem) {
-                Snackbar.make(flContainer, menuItem.getTitle() + " pressed", Snackbar.LENGTH_LONG).show();
-                menuItem.setChecked(true);
-                drawerLayout.closeDrawers();
-                return true;
-            }
-        });
+        navigationView.setNavigationItemSelectedListener(this);
 
         tvSearchGuide = (TextView) findViewById(R.id.tv_search_jokes);
         flContainer = (FrameLayout) findViewById(R.id.fl_container);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        if (savedInstanceState == null) {
-            jokeIndex++;
-            JokeFragment fragment = JokeFragment.newInstance(jokeIndex);
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.fl_container, fragment)
-                    .commit();
-
-        }
 
         btnNextJoke = (Button) findViewById(R.id.btn_next_joke);
         btnNextJoke.setOnClickListener(new View.OnClickListener() {
@@ -96,11 +69,7 @@ public class HomeActivity extends AppCompatActivity {
             public void onClick(View view) {
                 jokeIndex++;
                 if (jokeIndex < JokeTellerConstants.TOTAL_JOKES) {
-                    JokeFragment fragment = JokeFragment.newInstance(jokeIndex);
-                    getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.fl_container, fragment)
-                            .commit();
-
+                    navigateToJoke(jokeIndex);
                 } else {
                     jokeIndex = JokeTellerConstants.TOTAL_JOKES - 1;
                     Toast.makeText(getApplicationContext(), R.string.msg_no_more_joke, Toast.LENGTH_SHORT).show();
@@ -114,52 +83,24 @@ public class HomeActivity extends AppCompatActivity {
             public void onClick(View view) {
                 jokeIndex--;
                 if (jokeIndex >= 0) {
-                    JokeFragment fragment = JokeFragment.newInstance(jokeIndex);
-                    getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.fl_container, fragment)
-                            .commit();
+                    navigateToJoke(jokeIndex);
                 } else {
                     jokeIndex = 0;
                     Toast.makeText(getApplicationContext(), R.string.msg_no_more_joke, Toast.LENGTH_SHORT).show();
                 }
             }
         });
+
+        if (savedInstanceState == null) {
+            jokeIndex++;
+            navigateToJoke(jokeIndex);
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_home, menu);
-
-        MenuItem searchMenuItem = menu.findItem(R.id.action_search);
-        MenuItemCompat.setOnActionExpandListener(searchMenuItem, new MenuItemCompat.OnActionExpandListener() {
-            @Override
-            public boolean onMenuItemActionExpand(MenuItem item) {
-                tvSearchGuide.setVisibility(View.VISIBLE);
-                flContainer.setVisibility(View.INVISIBLE);
-                btnNextJoke.setVisibility(View.INVISIBLE);
-                btnPreviousJoke.setVisibility(View.INVISIBLE);
-                return true;
-            }
-
-            @Override
-            public boolean onMenuItemActionCollapse(MenuItem item) {
-                tvSearchGuide.setVisibility(View.INVISIBLE);
-                flContainer.setVisibility(View.VISIBLE);
-                btnNextJoke.setVisibility(View.VISIBLE);
-                btnPreviousJoke.setVisibility(View.INVISIBLE);
-                return true;
-            }
-        });
-
-        MenuItem shareItem = menu.findItem(R.id.action_share);
-        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
-        if (mShareActionProvider != null) {
-            mShareActionProvider.setShareIntent(createShareIntent());
-        } else {
-            Snackbar.make(flContainer, "ShareActionProvider is being null. Why ?", Snackbar.LENGTH_SHORT)
-                    .setAction("Action", null).show();
-        }
 
         return true;
     }
@@ -171,24 +112,67 @@ public class HomeActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        switch(id) {
+        switch (id) {
             case android.R.id.home:
                 drawerLayout.openDrawer(GravityCompat.START);
                 return true;
             case R.id.action_settings:
-                return true;
-            case R.id.action_love:
-                Toast.makeText(getApplicationContext(), getString(R.string.lbl_love_the_joke), Toast.LENGTH_SHORT).show();
                 return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    private static Intent createShareIntent(){
-        Intent myShareIntent = new Intent(Intent.ACTION_SEND);
-        myShareIntent.setType("text/*");
-        myShareIntent.putExtra(Intent.EXTRA_TEXT, "Hello Share Action Provider!");
-        return myShareIntent;
+    private void navigateToJoke(int jokeIndex) {
+        JokeFragment fragment = JokeFragment.newInstance(jokeIndex);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fl_container, fragment)
+                .commit();
+    }
+
+    private void navigateToPhandeeyar() {
+        Intent intent = EventActivity.newIntent();
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(final MenuItem menuItem) {
+        menuItem.setChecked(true);
+        drawerLayout.closeDrawers();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                switch (menuItem.getItemId()) {
+                    case R.id.left_menu_har_tha_pa_day_thar:
+                        //navigateToJoke(jokeIndex);
+                        break;
+                    case R.id.left_menu_phandeeyar:
+                        navigateToPhandeeyar();
+                        break;
+
+                }
+            }
+        }, 100); //to close drawer smoothly.
+
+        return true;
+    }
+
+    @Override
+    public boolean onMenuItemActionExpand(MenuItem item) {
+        tvSearchGuide.setVisibility(View.VISIBLE);
+        flContainer.setVisibility(View.INVISIBLE);
+        btnNextJoke.setVisibility(View.INVISIBLE);
+        btnPreviousJoke.setVisibility(View.INVISIBLE);
+        return true;
+    }
+
+    @Override
+    public boolean onMenuItemActionCollapse(MenuItem item) {
+        tvSearchGuide.setVisibility(View.INVISIBLE);
+        flContainer.setVisibility(View.VISIBLE);
+        btnNextJoke.setVisibility(View.VISIBLE);
+        btnPreviousJoke.setVisibility(View.VISIBLE);
+        return true;
     }
 }
--- app/src/main/java/xyz/aungpyaephyo/joketeller/fragments/JokeFragment.java
+++ app/src/main/java/xyz/aungpyaephyo/joketeller/fragments/JokeFragment.java
@@ -1,13 +1,22 @@
 package xyz.aungpyaephyo.joketeller.fragments;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import xyz.aungpyaephyo.joketeller.R;
 import xyz.aungpyaephyo.joketeller.data.models.JokeModel;
@@ -23,6 +32,10 @@ public class JokeFragment extends Fragment {
     private int jokeIndex;
     private JokeVO joke;
 
+    private ShareActionProvider mShareActionProvider;
+    private View mView;
+    private MenuItemCompat.OnActionExpandListener mOnActionExpandListener;
+
     public static JokeFragment newInstance(int jokeIndex) {
         JokeFragment fragment = new JokeFragment();
         Bundle bundle = new Bundle();
@@ -32,6 +45,13 @@ public class JokeFragment extends Fragment {
     }
 
     public JokeFragment() {
+
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mOnActionExpandListener = (MenuItemCompat.OnActionExpandListener) context;
     }
 
     @Override
@@ -42,22 +62,60 @@ public class JokeFragment extends Fragment {
             jokeIndex = bundle.getInt(BARG_JOKE_INDEX);
             joke = JokeModel.getInstance().getJoke(jokeIndex);
         }
+
+        setHasOptionsMenu(true);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        mView = inflater.inflate(R.layout.fragment_home, container, false);
 
-        TextView tvJokeTitle = (TextView) view.findViewById(R.id.tv_joke_title);
+        TextView tvJokeTitle = (TextView) mView.findViewById(R.id.tv_joke_title);
         tvJokeTitle.setText(joke.getJokeTitle());
 
-        TextView tvJokeContent = (TextView) view.findViewById(R.id.tv_joke_content);
+        TextView tvJokeContent = (TextView) mView.findViewById(R.id.tv_joke_content);
         tvJokeContent.setText(joke.getJokeContent());
 
-        ImageView ivJoke = (ImageView) view.findViewById(R.id.iv_joke);
+        ImageView ivJoke = (ImageView) mView.findViewById(R.id.iv_joke);
         ivJoke.setImageResource(joke.getJokeImage());
 
-        return view;
+        return mView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_joke_screen, menu);
+
+        MenuItem searchMenuItem = menu.findItem(R.id.action_search);
+        MenuItemCompat.setOnActionExpandListener(searchMenuItem, mOnActionExpandListener);
+
+        MenuItem shareItem = menu.findItem(R.id.action_share);
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(createShareIntent());
+        } else {
+            Snackbar.make(mView, "ShareActionProvider is being null. Why ?", Snackbar.LENGTH_SHORT)
+                    .setAction("Action", null).show();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_love:
+                Toast.makeText(getContext(), getString(R.string.lbl_love_the_joke), Toast.LENGTH_SHORT).show();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private static Intent createShareIntent(){
+        Intent myShareIntent = new Intent(Intent.ACTION_SEND);
+        myShareIntent.setType("text/*");
+        myShareIntent.putExtra(Intent.EXTRA_TEXT, "Hello Share Action Provider!");
+        return myShareIntent;
     }
 }
